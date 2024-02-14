cdbcc381f2cb97798b49e5bc9367ee40b239bf63
Muhammad Junaid <muhammad.junaid@adroittech.eu>
2024-02-14 18:15:36 +0500

    day 5- middlewares, logger, cors

--- Day 5/server.js
+++ Day 5/server.js
@@ -1,7 +1,39 @@
 const path = require("path");
+const { logger } = require("./middleware/logEvents.js");
 const express = require("express");
+const cors = require("cors");
+const { error } = require("console");
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// CUSTOM LOGGER MIDDLEWARE
+app.use(logger);
+// CROSS ORIGIN RESOURCE SHARING
+const whiteList = [
+  "https://www.google.com",
+  "http://127.0.0.1:5000",
+  "http://localhost:5000",
+  "http://localhost:5000/",
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+//BUILT-IN MIDDLEWARES
+// ACCEPTS DATA ENCODED IN URL. SUCH AS FROM HTML FORMS
+app.use(express.urlencoded({ extended: false }));
+// ACCEPT DATA IN THE JSON FORMAT
+app.use(express.json());
+// SERVE STATIC FILES
+app.use(express.static(path.join(__dirname, "./files")));
 
 app.get("^/$|index(.html)?", (req, res) => {
   //   res.sendFile("./files/index.html", { root: __dirname });
@@ -49,6 +81,6 @@ app.get("/old-page(.html)?", (req, res) => {
 
 // app.get("/hello(.html)?", [one, two, three]);
 
-app.listen(PORT, () => {
+app.listen(5000, () => {
   console.log("Server is running in the port ", PORT);
 });
